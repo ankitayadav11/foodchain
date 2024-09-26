391ee0df34d129fda38a45a9899a4f089a5c18b0
ankitayadav11 <ankitayadav0010@gmail.com>
2024-09-26 19:23:30 +0530

    I Change Navbar

--- src/Component/Header.js
+++ src/Component/Header.js
@@ -1,5 +1,14 @@
 export const Header = () => {
     return (
-        <div className = "text-purple-950"> This is Header Component </div>
+        <div className="navbar" >
+            <ul>
+        <li><a href="#home">Home</a></li>
+        <li><a href="#meus">Menu</a></li>
+        <li><a href="#contact">Contact</a></li>
+        <li><a href="#about">About</a></li>
+        <button>Log in</button>
+      </ul></div>   
+    
 )
 }
+   
